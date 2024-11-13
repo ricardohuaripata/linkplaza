b25131c1c1cb90181fd5b625adc18492ec180503
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2024-11-13 20:00:06 +0100

    feat: add not-found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { SignInComponent } from './pages/sign-in/sign-in.component';
 import { SignUpComponent } from './pages/sign-up/sign-up.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
   {
@@ -18,5 +19,10 @@ export const routes: Routes = [
     title: 'Sign up | LinkPlaza',
     path: 'auth/signup',
     component: SignUpComponent,
+  },
+  {
+    title: 'Not found | LinkPlaza',
+    path: '**',
+    component: NotFoundComponent,
   },
 ];
