8063a93be5695e6ff3057e0d5cf9a6a3c5cbce99
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2024-11-13 02:32:27 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { SignUpComponent } from './pages/sign-up/sign-up.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Welcome | LinkPlaza',
+    component: HomeComponent,
+  },
+  {
+    title: 'Sign in | LinkPlaza',
+    path: 'signin',
+    component: SignInComponent,
+  },
+  {
+    title: 'Sign up | LinkPlaza',
+    path: 'signup',
+    component: SignUpComponent,
+  },
+];
