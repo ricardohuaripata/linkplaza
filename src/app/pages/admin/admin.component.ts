df8209d0ff4e42e9026143c41a3e3f1d35984cb5
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2024-11-25 16:52:13 +0100

    refactor: info section componentization

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -10,17 +10,12 @@ import { Subscription } from 'rxjs';
 import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { User } from '../../interfaces/user';
 import { Page } from '../../interfaces/page';
-import {
-  FormBuilder,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { PageService } from '../../services/page/page.service';
+import { ReactiveFormsModule } from '@angular/forms';
 import { SocialLinksComponent } from './components/social-links/social-links.component';
 import { CustomLinksComponent } from './components/custom-links/custom-links.component';
 import { Router } from '@angular/router';
-import { CustomizationComponent } from "./components/customization/customization.component";
+import { CustomizationComponent } from './components/customization/customization.component';
+import { InfoComponent } from './components/info/info.component';
 
 @Component({
   selector: 'app-admin',
@@ -30,33 +25,22 @@ import { CustomizationComponent } from "./components/customization/customization
     CommonModule,
     SocialLinksComponent,
     CustomLinksComponent,
-    CustomizationComponent
-],
+    CustomizationComponent,
+    InfoComponent,
+  ],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.scss',
 })
 export class AdminComponent implements OnInit, OnDestroy {
-  private subscription: Subscription = new Subscription();
   authUser?: User;
   targetPage?: Page;
-  openEditPageModal: boolean = false;
-  editPageForm: FormGroup;
-  disableForm: boolean = false;
-  editPageFormSubmitFeedbackMessage?: string;
+  private subscription: Subscription = new Subscription();
 
   constructor(
     private userService: UserService,
-    private pageService: PageService,
     @Inject(PLATFORM_ID) private platformId: Object,
-    private fb: FormBuilder,
     private router: Router
-  ) {
-    this.editPageForm = this.fb.group({
-      title: ['', Validators.maxLength(32)],
-      bio: ['', Validators.maxLength(256)],
-      pictureUrl: ['', Validators.maxLength(3200)],
-    });
-  }
+  ) {}
 
   ngOnInit(): void {
     if (isPlatformBrowser(this.platformId)) {
@@ -67,13 +51,7 @@ export class AdminComponent implements OnInit, OnDestroy {
             this.authUser = response.data;
             this.targetPage = response.data.pages[0];
 
-            if (this.targetPage) {
-              this.editPageForm.setValue({
-                title: this.targetPage?.title,
-                bio: this.targetPage?.bio,
-                pictureUrl: this.targetPage?.pictureUrl,
-              });
-            } else {
+            if (!this.targetPage) {
               this.router.navigate(['/new-page']);
             }
           },
@@ -81,32 +59,6 @@ export class AdminComponent implements OnInit, OnDestroy {
         })
       );
     }
-  }
-
-  onEditPageFormSubmit(id: number) {
-    this.disableForm = true;
-
-    const requestBody: any = {
-      title: this.editPageForm.value.title,
-      bio: this.editPageForm.value.bio,
-      pictureUrl: this.editPageForm.value.pictureUrl,
-    };
-
-    this.subscription.add(
-      this.pageService.updatePage(id, requestBody).subscribe({
-        next: (response: any) => {
-          this.targetPage = response.data;
-          this.disableForm = false;
-          this.openEditPageModal = false;
-        },
-        error: (event) => {
-          this.editPageFormSubmitFeedbackMessage = event.error.message;
-          setTimeout(() => {
-            this.disableForm = false;
-          }, 3000);
-        },
-      })
-    );
   }
 
   ngOnDestroy(): void {
---
+++ src/app/pages/admin/components/info/info.component.ts
@@ -0,0 +1,76 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { Page } from '../../../../interfaces/page';
+import { PageService } from '../../../../services/page/page.service';
+
+@Component({
+  selector: 'app-info',
+  standalone: true,
+  imports: [ReactiveFormsModule, CommonModule],
+  templateUrl: './info.component.html',
+  styleUrl: './info.component.scss',
+})
+export class InfoComponent implements OnInit, OnDestroy {
+  @Input() page!: Page;
+  disableForm: boolean = false;
+
+  openEditPageModal: boolean = false;
+  editPageForm: FormGroup;
+
+  editPageFormSubmitFeedbackMessage?: string;
+
+  private subscription: Subscription = new Subscription();
+
+  constructor(private pageService: PageService, private fb: FormBuilder) {
+    this.editPageForm = this.fb.group({
+      title: ['', Validators.maxLength(32)],
+      bio: ['', Validators.maxLength(256)],
+      pictureUrl: ['', Validators.maxLength(3200)],
+    });
+  }
+
+  ngOnInit(): void {
+    this.editPageForm.setValue({
+      title: this.page?.title,
+      bio: this.page?.bio,
+      pictureUrl: this.page?.pictureUrl,
+    });
+  }
+
+  onEditPageFormSubmit() {
+    this.disableForm = true;
+
+    const requestBody: any = {
+      title: this.editPageForm.value.title,
+      bio: this.editPageForm.value.bio,
+      pictureUrl: this.editPageForm.value.pictureUrl,
+    };
+
+    this.subscription.add(
+      this.pageService.updatePage(this.page.id, requestBody).subscribe({
+        next: (response: any) => {
+          this.page = response.data;
+          this.disableForm = false;
+          this.openEditPageModal = false;
+        },
+        error: (event) => {
+          this.editPageFormSubmitFeedbackMessage = event.error.message;
+          setTimeout(() => {
+            this.disableForm = false;
+          }, 3000);
+        },
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+}
