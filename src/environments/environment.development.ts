6b74e68099f3d4ba0458b4a11ad0727bab1e0069
Ricardo Huaripata <102578959+ricardohuaripata@users.noreply.github.com>
2024-12-22 21:02:59 +0100

    update env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  API_URL: 'http://localhost:8080',
+  API_URL: 'https://linkplaza-api.up.railway.app',
   APP_NAME: 'linkplaza.netlify.app',
   BASE_URL: 'https://linkplaza.netlify.app',
   API_KEY: 'TBUlS0ABFVcb7zbWZWa9h0GhDUN6cIutir7YKR4VOmFRLsoS6efimuQWKrqTVRwB',
